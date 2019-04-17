cdecf3a9dd3a0e32731ee21c6bb030ebf643619e
David_JonesDVN <gmdavidjones@gmail.com>
2019-04-17 19:54:49 +0430

    Fixed context menu crash.
    Thanks .NET C# for not remembering what control triggered the context menu in the first place.

--- Touhou Launcher/MainForm.cs
+++ Touhou Launcher/MainForm.cs
@@ -593,7 +593,7 @@ namespace Touhou_Launcher
 
         private void configureToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Button btn = (Button)((ContextMenuStrip)((ToolStripMenuItem)sender).GetCurrentParent()).SourceControl;
+            Button btn = (Button)((ContextMenuStrip)((ToolStripMenuItem)sender).GetCurrentParent()).Tag;
             ConfigForm gameConfig = new ConfigForm(btn);
             gameConfig.ShowDialog();
         }
